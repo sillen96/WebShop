51336d8b5fe6a977380b25111294d1f39ce21035
sillen96 <78109230+sillen96@users.noreply.github.com>
2021-11-09 13:34:55 +0100

    Test

--- Console.App/WebShopConsoleApp/WebShopConsoleApp/Program.cs
+++ Console.App/WebShopConsoleApp/WebShopConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace WebShopConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("God morgon"); 
         }
     }
 }
