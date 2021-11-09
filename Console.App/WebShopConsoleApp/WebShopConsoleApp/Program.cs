fa13b800f0f347f3c257d860dbd7390912d3854f
KayJayTee <kj.tjust@protonmail.com>
2021-11-09 10:39:00 +0100

    Grunden Färdiglagd
    Det ska vara klart för att starta arbete nu ifall man vill; uppdatera mig om ni märker något fel eller om mappar eller filer bör om-organiseras.
    
    LETS GOO!!!!11

---
+++ Console.App/WebShopConsoleApp/WebShopConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebShopConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
