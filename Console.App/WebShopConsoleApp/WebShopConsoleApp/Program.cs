174cff9626c24dbc2c2095bfdc6a5ce68730d264
KayJayTee <kj.tjust@protonmail.com>
2021-11-09 15:36:04 +0100

    fånigt test

--- Console.App/WebShopConsoleApp/WebShopConsoleApp/Program.cs
+++ Console.App/WebShopConsoleApp/WebShopConsoleApp/Program.cs
@@ -4,11 +4,19 @@ namespace WebShopConsoleApp
 {
     class Program
     {
+
+        public string SatanDies()
+        {
+            Console.WriteLine("satan fkn dies");
+
+            return SatanDies();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("God morgon HEJ  vGD"); 
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Yeeeeeehaaaaa")
+            Console.WriteLine("Yeeeeeehaaaaa");
+            
         }
     }
 }
