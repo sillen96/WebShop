b887b6716297651618111b74016c821d5f213f01
KayJayTee <kj.tjust@protonmail.com>
2021-11-09 14:51:48 +0100

    Test

--- Console.App/WebShopConsoleApp/WebShopConsoleApp/Program.cs
+++ Console.App/WebShopConsoleApp/WebShopConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace WebShopConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Yeeeeeehaaaaa")
         }
     }
 }
