7b6b411f8d7bd2cc179f7bb63fdbfad961929689
KayJayTee <kj.tjust@protonmail.com>
2021-11-10 14:45:54 +0100

    Arbetat med List
    Arbetar med List<> I Consollen för Produkter och inplanterat Models i RazorPages Projektet.
    
    List<> i Console.App har några fel fortfarande, om ni hittar en lösning får ni gärna rätta dem annars kommer jag klura på det vid senare tillfälle.
    
    Tar ett avbrott nu och fortsätter senare.

--- Console.App/WebShopConsoleApp/WebShopConsoleApp/Program.cs
+++ Console.App/WebShopConsoleApp/WebShopConsoleApp/Program.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace WebShopConsoleApp
@@ -6,6 +5,7 @@ namespace WebShopConsoleApp
 
     class Product //Superclass; Models
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Cost { get; set; }
@@ -21,6 +21,7 @@ namespace WebShopConsoleApp
 
     class Furniture : Product
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Cost { get; set; }
@@ -29,9 +30,10 @@ namespace WebShopConsoleApp
         public double Weight { get; set; }
         public string Warning { get; set; }
 
-        public Furniture(string name, string description, decimal cost,
+        public Furniture(int id, string name, string description, decimal cost,
         double width, double height, double weight, string warning)
         {
+            ID = id;
             Name = name;
             Description = description;
             Cost = cost;
@@ -46,15 +48,17 @@ namespace WebShopConsoleApp
 
     class Elixir : Product
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Cost { get; set; }
         public string[] Ingredients { get; set; }
         public string Warning { get; set; }
 
-        public Elixir(string name, string description, decimal cost,
+        public Elixir(int id, string name, string description, decimal cost,
         string[] ingredients, string warning)
         {
+            ID = id;
             Name = name;
             Description = description;
             Cost = cost;
@@ -66,15 +70,17 @@ namespace WebShopConsoleApp
 
     class Tobacco : Product
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Cost { get; set; }
         public string[] Ingredients { get; set; }
         public string Warning { get; set; }
 
-        public Tobacco(string name, string description, decimal cost,
-        double width, double height, double weight, string[] ingredients, string warning)
+        public Tobacco(int id, string name, string description, decimal cost,
+        string[] ingredients, string warning)
         {
+            ID = id;
             Name = name;
             Description = description;
             Cost = cost;
@@ -83,18 +89,51 @@ namespace WebShopConsoleApp
         }
 
     }
+
+
     class Program
     {
 
+        void Generate
 
         static void Main(string[] args)
         {
             List<Product> products = new List<Product>();
 
-            products.Add(new Elixir(name, description, cost, ingredients, warning)); //Koden fungerar;
-                                                                                     //men har ingen hänvisning då detaljerna ännu inte har några värden eller strängar.
-          
+
+            products.Add(
+            new Furniture
+            {
+            ID = 02,
+            Name = "Lergoblin",
+            Description = "En liten figur som sjunger tvåstämmigt den kända ”Kloaksonaten” – inte särskilt vacker melodi, men underhållande!",
+            Cost = 50,
+            Width = 0.3d,
+            Height = 0.3d,
+            Weight = 0.2d,
+            Warning = "Varning: En tyst läge inkluderas inte i produkten."
+        });
+            products.Add(
+            new Elixir
+            {
+                ID = 01,
+                Name = "Blyfot",
+                Description = "Denn flaskan har just nu ingen bestämt text, men det kommer!",
+                Cost = 100,
+                Ingredients = ("En krossad tånagel från ett troll", "tre nypor trollört", "svett från en jätte"), //Försöker komma på  hur man ska implantera en array-lösning här
+                Warning = "Biverkningar kan förekomma: Hög aptit, försämrad balansförmåga, en stor...fot"
+            });
+
+
+
 
         }
     }
 }
+
+/*      ID = id;
+        Name = name;
+        Description = description;
+        Cost = cost;
+        Ingredients = ingredients;
+        Warning = warning;*/
