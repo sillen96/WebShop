0d8d62a6151e4f0455380662342fa77206caf39c
KayJayTee <kj.tjust@protonmail.com>
2021-11-10 10:08:55 +0100

    Pågående arbete med Superclass
    arbetar i console.app med att få till en superclass

--- Console.App/WebShopConsoleApp/WebShopConsoleApp/Program.cs
+++ Console.App/WebShopConsoleApp/WebShopConsoleApp/Program.cs
@@ -1,21 +1,75 @@
 using System;
+using System.Collections.Generic;
 
 namespace WebShopConsoleApp
 {
+
+    class Product //Superclass; Models
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string[] Ingredients { get; set; }
+        public string Warning { get; set; }
+
+        public Product(string name, string description, decimal cost,
+        double width, double height, double weight, string[] ingredients, string warning)
+        {
+            Name = name;
+            Description = description;
+            Cost = cost;
+            Width = width;
+            Height = height;
+            Weight = weight;
+            Ingredients = ingredients;
+            Warning = warning;
+        }
+    }
+
+
+
+    class Furniture : Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string Warning { get; set; }
+
+
+    }
+
+    class Elixir : Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+        public string[] Ingredients { get; set; }
+        public string Warning { get; set; }
+
+    }
+
+    class Tobacco : Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+        public string[] Ingredients { get; set; }
+        public string Warning { get; set; }
+
+    }
     class Program
     {
 
-        public string SatanDies()
-        {
-            Console.WriteLine("satan fkn dies");
-
-            return SatanDies();
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("God morgon HEJ  vGD"); 
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Yeeeeeehaaaaa");
+            List<Product> products = new List<Product>();
+
             
         }
     }
